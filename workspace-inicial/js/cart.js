7b57da1d595e353eba588f7237e3eab9292c3549
masteraised <leonxgx@gmail.com>
2023-10-23 17:03:58 -0300

    ent6

--- workspace-inicial/js/cart.js
+++ workspace-inicial/js/cart.js
@@ -1,6 +1,3 @@
-const userID = 25801;
-const url = `${CART_INFO_URL}${userID}.json`;
-
 //Definimos una función para generar las filas
 function createQueue(element) {
   const queue = document.createElement("tr");
@@ -13,7 +10,10 @@ function createQueue(element) {
     }" class="col-sm-2 w-75 mx-auto form-control-sm cantidad-input"  min="0"></td>
         <td class="subtotal-cell text-success">${element.unitCost * element.count} ${element.currency
     }</td>
-        <td> <button class="delete-product" data-product-id="${element.id}" >Eliminar</button></td>
+        <td> <button class="delete-product" data-product-id="${element.id}" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+      </svg></button></td>
     `;
   return queue;
 }
@@ -32,26 +32,6 @@ document.addEventListener("DOMContentLoaded", () => {
     subtotalCell.textContent = subtotal.toFixed(0) + " USD";
   }
 
-  // Trae los datos del carrito
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const productInfoDiv = document.getElementById("cart-products");
-      const product = data.articles[0];
-
-      // Creamos una fila con los datos del producto
-      let queue = createQueue(product);
-
-      // Agregamos la fila a la tabla
-      productInfoDiv.appendChild(queue);
-
-      // Agregamos un evento "input" al campo de cantidad en esta fila
-      const cantidadInput = queue.querySelector(".cantidad-input");
-      cantidadInput.addEventListener("input", () => calcularSubtotal(queue));
-    })
-    .catch((error) => {
-      console.error("Error al cargar el carrito de compras:", error);
-    });
   displayCartItems();
 
 
@@ -100,7 +80,8 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   }
-  const overlay = document.getElementById("overlay");
+
+    const overlay = document.getElementById("overlay");
     const mostrarOverlayButton = document.getElementById("mostrarOverlay");
     const cerrarOverlayButton = document.getElementById("cerrarOverlay");
   
@@ -115,4 +96,34 @@ document.addEventListener("DOMContentLoaded", () => {
     cerrarOverlayButton.addEventListener("click", () => {
       overlay.classList.remove("active");
     });
+    
+
+    /*  SE PUEDE BORRAR (es un intento de la pauta 1)
+    const subTotal = document.getElementsByClassName("subtotal-cell").value;
+
+    subTotalytys = [];
+    array.forEach(element => {
+     
+    }); */
+
 });
+
+
+
+/* 
+Agrega un espacio donde se visualicen:
+
+El subtotal general: la suma de los subtotales (costo por cantidad) de todos los artículos
+El costo de envío: calculado a partir del envío seleccionado por el usuario (5%, 7% o 15%) y siendo un porcentaje del valor anterior (el subtotal).
+
+El total a pagar: la suma de los dos valores anteriores.
+Los 3 valores deberán actualizarse en tiempo real cuando se modifique el tipo de envío o los artículos en el carrito.
+
+Todos los valores deberán ser mostrados en dólares.
+
+
+if (currency == "UYU") {
+  
+} else {
+  
+}  */
