2a27f2e8cf6cba6f227ac552402633431ff2d36c
masteraised <leonxgx@gmail.com>
2023-10-31 15:48:40 -0300

    ent 7 hasta parte 3

--- workspace-inicial/js/my-profile.js
+++ workspace-inicial/js/my-profile.js
@@ -1,31 +1,65 @@
-function cerrarSesion() {
-  localStorage.removeItem("isLoggedIn"); // Eliminar el valor de inicio de sesión
-  localStorage.removeItem("username"); // Eliminar el nombre de usuario
-  window.location.href = "login.html"; // Redirige al inicio de sesión
-}
-// Obtén los elementos necesarios
-const perfilButton = document.getElementById("perfil-button");
-const perfilMenu = document.getElementById("perfil-menu");
-// Agrega un evento click al botón de perfil
-perfilButton.addEventListener("click", () => {
-// Cambia la visibilidad del menú desplegable
-if (perfilMenu.style.display === "block") {
-  perfilMenu.style.display = "none";
-} else {
-  perfilMenu.style.display = "block";
-}
-});
-// Cierra el menú si se hace clic en cualquier parte del documento
-document.addEventListener("click", (event) => {
-if (!perfilButton.contains(event.target) && !perfilMenu.contains(event.target)) {
-  perfilMenu.style.display = "none";
-}
+// Boostrap Utility: Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+
+// Listener para click button send y guardar datos de formulario, entrega7
+
+const formButton = document.getElementById('saveChanges')
+const firstName = document.getElementById('nameInput1')
+const lastName = document.getElementById('lastName1')
+const phoneNumber = document.getElementById('phoneNumber')
+const eMail = localStorage.getItem('username');
+
+formButton.addEventListener("click", (event) => {
+    
+   
+   
+    if (firstName.value !== '' && lastName.value !== '' && phoneNumber.value !== '') {
+
+        localStorage.setItem('firstName', firstName.value);
+        localStorage.setItem('secondName', nameInput2.value);
+        localStorage.setItem('lastName', lastName.value);
+        localStorage.setItem('lastName2', lastName2.value);
+        localStorage.setItem('phoneNumber', phoneNumber.value);
+      
+        
+    }
 });
 
-// Mostrar el nombre del usuario si está autenticado
-document.addEventListener("DOMContentLoaded", function(){
-const username = localStorage.getItem("username");
-if (username) {
-  perfilButton.textContent = `${username}`;
-}
-});
+const name1 = localStorage.getItem('firstName')
+const name2 = localStorage.getItem('secondName')
+const surname = localStorage.getItem('lastName')
+const surname2 = localStorage.getItem('lastName2')
+const phone = localStorage.getItem('phoneNumber')
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    phoneNumber.value = phone;
+    lastName.value = surname;
+    firstName.value = name1;
+    nameInput2.value = name2;
+    lastName2.value = surname2;
+
+
+})
+
+// Listener para click button send y guardar datos de formulario, entrega7
+
